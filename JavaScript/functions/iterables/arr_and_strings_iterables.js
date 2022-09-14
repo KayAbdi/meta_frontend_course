587cd99c34070df5c59e522439f9ab6957e7bf4d
Kienaan Abdi <abdifiiq@google.com>
2022-09-14 13:00:31 -0500

    methods and iterables

---
+++ JavaScript/functions/iterables/arr_and_strings_iterables.js
@@ -0,0 +1,29 @@
+// Arrays are iterable
+
+var veggies = ['onion', 'parsley', 'carrot'];
+
+console.log(veggies.length);
+
+console.log(veggies[0]);
+console.log(veggies[1]);
+
+for(var i = 0; i < veggies.length; i++){
+    console.log(veggies[i]);
+};
+
+// strings are iterable, too
+var greeting = 'Howdy';
+
+console.log(greeting.length);
+
+console.log(greeting[0]);
+console.log(greeting[1]);
+
+for(var i = 0; i < greeting.length; i++){
+    console.log(greeting[i]);
+};
+
+
+//but strings are not arrays thus you can't use array methods such as pop() and push() on strings 
+var greet = 'Hello';
+// greet.pop(); won't work here
---
+++ JavaScript/functions/methods/mathRand.js
@@ -0,0 +1,11 @@
+var rand = Math.random();
+console.log(rand);
+
+// ceil can only round upwards
+var rounded = Math.ceil(1.01);
+console.log(rounded);
+
+//Random integer and ceil it
+var decimal = Math.random()*10;
+var rounded2 = Math.ceil(decimal);
+console.log(rounded2);
