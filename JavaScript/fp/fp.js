e692ba8d09c85811b192d30353c8d940eb5945f0
Kienaan Abdi <abdifiiq@google.com>
2022-09-15 10:43:02 -0500

    added currency converter example

---
+++ JavaScript/fp/fp.js
@@ -0,0 +1,12 @@
+// functional programming 
+
+var currencyOne = 100;
+var currencyTwo = 0;
+var exchangeRate = 1.2;
+
+function convertCurrency(amount, rate){
+    return amount * rate;
+};
+
+currencyTwo = convertCurrency(currencyOne, exchangeRate);
+console.log(currencyTwo);
