81bb97a2759730a69076f6fc806164db49a2e1e5
Kienaan Abdi <abdifiiq@google.com>
2022-09-16 14:58:45 -0500

    object inheritance example added.

---
+++ JavaScript/OOP/classHierarchy.js
@@ -0,0 +1,41 @@
+class Animal {
+    // constructor: color, energy
+    // isActive()
+        // if energy > 0, energy -=20, console log energy
+        // else if energy <= 0, sleep()
+    // sleep()
+        // energy += 20
+        // console.log energy
+}
+class Cat extends Animal {
+    // constructor: sound, canJumpHigh, canClimbTrees, color, energy
+    // makeSound()
+        // console.log sound
+}
+class Bird extends Animal {
+    // constructor: sound, canFly, color, energy
+    // makeSound()
+        // console.log sound
+}
+class HouseCat extends Cat {
+    // constructor: houseCatSound, sound, canJumpHigh, canClimbTrees, color, energy
+    // makeSound(option)
+        // if (option)
+            // super.makeSound()
+        // console.log(houseCatSound)
+}
+class Tiger extends Cat {
+    // constructor: tigerSound, sound, canJumpHigh, canClimbTrees, color, energy
+    // makeSound(option)
+        // if (option)
+            // super.makeSound()
+        // console.log(tigerSound)
+}
+class Parrot extends Bird {
+    // constructor: canTalk, sound, canJumpHigh, canClimbTrees, color, energy
+    // makeSound(option)
+        // if (option)
+            // super.makeSound()
+        // if (canTalk)
+            // console.log("talking!")
+}
