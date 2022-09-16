ad429c628f013a61b9e75f221a80b41e73330a04
Kienaan Abdi <abdifiiq@google.com>
2022-09-16 11:10:28 -0500

    OOP exercises

---
+++ JavaScript/OOP/oop.js
@@ -0,0 +1,26 @@
+// polymorphism using classes
+
+class Bird {
+    useWings() {
+        console.log("Flying!")
+    }
+}
+class Eagle extends Bird {
+    useWings() {
+        super.useWings()
+        console.log("Barely flapping!")
+    }
+}
+class Penguin extends Bird {
+    useWings() {
+        console.log("Diving!")
+    }
+}
+var baldEagle = new Eagle();
+var kingPenguin = new Penguin();
+baldEagle.useWings(); // "Flying! Barely flapping!"
+kingPenguin.useWings(); // "Diving!"
+
+var date = new Date();
+console.log(date, ":", Math.pow(2,5));
+
--- JavaScript/fp/fp.js
+++ JavaScript/fp/fp.js
@@ -21,3 +21,46 @@ function example(){
     example();
 };
 example();
+
+
+// get distnace using fuction programing
+function getDistance(mph, h) {
+    return mph * h
+}
+var mph = 60;
+var h = 2;
+var distance = getDistance(mph, h);
+console.log(distance);
+
+// Object Oriented Programing example (OOP)
+var virtualPet = {
+    sleepy: true,
+    nap: function() {
+        this.sleepy = false
+    }
+}
+console.log(virtualPet.sleepy) // true
+virtualPet.nap()
+console.log(virtualPet.sleepy) // false
+
+// first class functions 
+function addTwoNums(a, b) {
+    console.log(a + b)
+}
+
+function randomNum() {
+    return Math.floor((Math.random() * 10) + 1);
+}
+function specificNum() { return 42 };
+
+var useRandom = true;
+
+var getNumber;
+
+if(useRandom) {
+    getNumber = randomNum
+} else {
+    getNumber = specificNum
+}
+
+addTwoNums(getNumber(), getNumber())
