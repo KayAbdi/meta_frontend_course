6175534f01240ddff6665ddf6c3125febdd64ede
Kienaan Abdi <abdifiiq@google.com>
2022-09-25 19:20:18 -0500

    correction

--- JavaScript/arrays_and_objects/exercise.js
+++ JavaScript/arrays_and_objects/exercise.js
@@ -5,9 +5,21 @@
  */
 
 // Your code here.
-function deepEqual(obj1, obj2){
-    if (obj1 === obj2 && obj1.keys === obj2.keys) return true;
-    else return false;
+function deepEqual(a, b) {
+    if (a === b) return true;
+    
+    if (a == null || typeof a != "object" ||
+        b == null || typeof b != "object") return false;
+  
+    let keysA = Object.keys(a), keysB = Object.keys(b);
+  
+    if (keysA.length != keysB.length) return false;
+  
+    for (let key of keysA) {
+      if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+    }
+  
+    return true;
   }
   let obj = {here: {is: "an"}, object: 2};
   console.log(deepEqual(obj, obj));
